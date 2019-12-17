2ce983ac40292ddb5b618db412655a631e8e6ff4
Kaan Türeyyen <45270507+kaantureyyen@users.noreply.github.com>
2019-12-17 18:37:47 +0100

    Vuex Persisted State

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import auth from './modules/auth'
 import user from './modules/user'
 
@@ -9,5 +10,10 @@ export default new Vuex.Store({
   modules: {
     auth,
     user
-  }
+  },
+  plugins: [
+    createPersistedState({
+      paths: ['auth', 'user']
+    })
+  ]
 })
