f4e2cf7a962074c3232fcc3d746f8629b93b6d9b
Ivan <lukac.ivan94@gmail.com>
2020-01-02 16:45:12 +0100

    changed username

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: {
+    email: { //
         type: String,
          required: true,
           //unique: true,
