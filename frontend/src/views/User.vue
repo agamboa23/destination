e4ee24edf4b34b0a4a602339caeb9b68e82f1af2
Kaan Türeyyen <45270507+kaantureyyen@users.noreply.github.com>
2020-01-11 17:44:28 +0100

    Better User Page

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -1,24 +1,24 @@
 <template>
-  <div v-if="dataReady">
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="10" md="8">
-        <user-card :userId="id" />
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col cols="5" v-for="(item, index) in trips" :key="index">
-        <trip-request
-          :tripId="item._id"
-          :requests="item.requests"
-          :members="item.members"
-          :maxMembers="item.number_of_members"
-          :origin="item.origin"
-          :destination="item.destination"
-          :date="item.date_of_trip"
-        />
-      </v-col>
-    </v-row>
-  </div>
+  <v-row align="start" justify="center">
+    <v-col cols="12" sm="10" md="8">
+      <user-card :userId="id" />
+    </v-col>
+    <v-col cols="12">
+      <v-row align="center" justify="center">
+        <v-col cols="5" v-for="(item, index) in trips" :key="index">
+          <trip-request
+            :tripId="item._id"
+            :requests="item.requests"
+            :members="item.members"
+            :maxMembers="item.number_of_members"
+            :origin="item.origin"
+            :destination="item.destination"
+            :date="item.date_of_trip"
+          />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -35,20 +35,18 @@ export default {
   },
   data: () => {
     return {
-      dataReady: false,
       trips: []
     }
   },
   computed: {
     ...mapState('user', ['id'])
   },
-  async mounted() {
+  async created() {
     const res = await axios.get(
       'http://localhost:3000/trips/upcoming/' + this.id
     )
     const resData = res.data.trips
     this.trips = resData
-    this.dataReady = true
   }
 }
 </script>
